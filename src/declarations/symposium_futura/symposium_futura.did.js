1ab9a3ecc66f60e59415d940a738c495b6b34ff5
Ioannis Mourginakis <tiodargy@Ioanniss-MacBook-Pro.local>
2022-05-25 16:33:42 -0700

    make primitive backend, can handle pseudonyms

--- src/declarations/symposium_futura/symposium_futura.did.js
+++ src/declarations/symposium_futura/symposium_futura.did.js
@@ -1,6 +1,7 @@
 export const idlFactory = ({ IDL }) => {
   return IDL.Service({
     'greet' : IDL.Func([IDL.Text], [IDL.Text], []),
+    'wallet_balance' : IDL.Func([], [IDL.Nat], []),
     'whoami' : IDL.Func([], [IDL.Principal], []),
   });
 };
