28b13e6ad2a3d40a78141db858840dffe9f4d90a
Ioannis Mourginakis <tiodargy@Ioanniss-MacBook-Pro.local>
2022-05-26 00:46:00 -0700

    fix navbar css and add box-shadow to user-card

--- src/declarations/symposium_futura/symposium_futura.did.d.ts
+++ src/declarations/symposium_futura/symposium_futura.did.d.ts
@@ -1,6 +1,10 @@
 import type { Principal } from '@dfinity/principal';
 export interface _SERVICE {
-  'greet' : (arg_0: string) => Promise<string>,
+  'get_all_posts' : () => Promise<boolean>,
+  'get_pseudonym' : () => Promise<string>,
+  'make_post' : () => Promise<undefined>,
+  'update_pseudonym' : (arg_0: string) => Promise<undefined>,
   'wallet_balance' : () => Promise<bigint>,
+  'wallet_receive' : () => Promise<{ 'accepted' : bigint }>,
   'whoami' : () => Promise<Principal>,
 }
--- src/declarations/symposium_futura/symposium_futura.did.js
+++ src/declarations/symposium_futura/symposium_futura.did.js
@@ -1,7 +1,15 @@
 export const idlFactory = ({ IDL }) => {
   return IDL.Service({
-    'greet' : IDL.Func([IDL.Text], [IDL.Text], []),
+    'get_all_posts' : IDL.Func([], [IDL.Bool], ['query']),
+    'get_pseudonym' : IDL.Func([], [IDL.Text], ['query']),
+    'make_post' : IDL.Func([], [], []),
+    'update_pseudonym' : IDL.Func([IDL.Text], [], []),
     'wallet_balance' : IDL.Func([], [IDL.Nat], []),
+    'wallet_receive' : IDL.Func(
+        [],
+        [IDL.Record({ 'accepted' : IDL.Nat64 })],
+        [],
+      ),
     'whoami' : IDL.Func([], [IDL.Principal], []),
   });
 };
