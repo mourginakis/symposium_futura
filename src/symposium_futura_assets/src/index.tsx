98ee26fc10daa4bd9683fc29cdad73e9c4f4dd70
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2022-05-30 15:34:07 -0700

    get auth working, but not between page refreshes

--- src/symposium_futura_assets/src/index.tsx
+++ src/symposium_futura_assets/src/index.tsx
@@ -4,7 +4,7 @@ import { AuthClient } from "@dfinity/auth-client";
 import { Actor, Identity, ActorSubclass, /*HttpAgent*/ } from "@dfinity/agent";
 
 
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState, useContext} from "react";
 import ReactDOM from "react-dom/client";
 import exampleposts from "../assets/exampleposts.json";
 import '../assets/css/symposium.scss';
@@ -15,35 +15,59 @@ const audio = new Audio("../ill-find-you-crystal-river-inmysleep.mp3");
 
 
 
+
+const AuthContext = React.createContext({
+  authClient: null,
+  isAuthenticated: false,
+  identity: null,
+  futura_actor: null
+});
+
+
 function App() {
   const [authClient, setAuthClient] = useState<AuthClient>();
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>();
   const [identity, setIdentity] = useState<Identity>();
   const [futura_actor, setFutura_Actor] = useState<ActorSubclass<_SERVICE>>();
 
+  const [myData, setMyData] = useState<number>(0);
+  const [myString, setMyString] = useState<string>();
+
   console.log("rerunning function!!!!");
+
+  useEffect(() => {
+
+  });
 
 
   useEffect(() => {
     const init = async () => {
-      const authClient1 = await AuthClient.create();
-      const authentic = await authClient1.isAuthenticated();
-      setAuthClient(authClient1);
-      setIsAuthenticated(authentic);
-      console.log("probably not authenticated");
+      // works even on page refresh
+      const temp_authClient = await AuthClient.create();
+      const temp_isAuthenticated = await temp_authClient.isAuthenticated();
+      setAuthClient(temp_authClient);
+      setIsAuthenticated(temp_isAuthenticated);
+
+      // // kinda works?? but not on page refresh
+      // setAuthClient(await AuthClient.create())
+      // console.log(authClient);
+      // setIsAuthenticated(await authClient?.isAuthenticated())
+      // console.log(isAuthenticated)
+
     };
     init();
   }, []);
 
 
-  const authorize = async () => {
+  const authenticate = async () => {
+    console.log("attempting authenticate");
     const days = BigInt(1);
     const hours = BigInt(24);
     const nanoseconds = BigInt(3600000000000);
 
     await authClient?.login({
       onSuccess: async () => {
-        console.log("logged in");
+        console.log("successful login");
         setIsAuthenticated(true);
         handleAuthenticated(authClient);
       },
@@ -60,63 +84,104 @@ function App() {
 
   const handleAuthenticated = async (authClient: AuthClient) => {
     const identity1 = (await authClient?.getIdentity()) as unknown as Identity;
+    const identity = identity1;
     setIdentity(identity1);
+
 
     const futura_actor1 = createActor(canisterId as string, {
       agentOptions: {
         identity,
       },
     });
+
     setFutura_Actor(futura_actor1);
+    
+    // const response = await futura_actor1?.whoami();
+    // console.log(response?.toString())
 
     // Invalidate identity then render login when user goes idle
     authClient?.idleManager?.registerCallback(() => {
       Actor.agentOf(futura_actor1)?.invalidateIdentity?.();
       //renderIndex();
+      console.log("user timeout");
     });
   };
 
-  const deauthorize = async() => {
+  const deauthenticate = async() => {
     authClient?.logout();
     setIsAuthenticated(false)
   }
 
 
   // Rendering 
-  if (!isAuthenticated) {
     return (
-      <div>
-      <h1>YU are not authenticated!!</h1>
-      <button onClick={authorize}>Authorize</button>
-      <button onClick={deauthorize}>deAuthorize</button>
-      </div>
+      <AuthContext.Provider value={{
+        authClient: authClient as any,
+        isAuthenticated: isAuthenticated as boolean,
+        identity: identity as any,
+        futura_actor: futura_actor as any,
+        }}>
+        <div>
+          <h1>authenticate options</h1>
+          Counter
+          <button onClick={() => setMyData(myData + 1)}>
+            Click me
+            </button>
+          {myData}
+          <br / >
+            <h4>State</h4>
+            {"authClient:   " + authClient} <br / >
+            {"isAuthenticated:  " + isAuthenticated} <br />
+            {"identity:  " + identity} <br / >
+            {"actor:  " + futura_actor + ", tostring: " + futura_actor?.toString()} <br />
+            <br />
+          {"authClient.getIdentity().getPrincipal():    " + String(authClient?.getIdentity().getPrincipal())}<br />
+          <button onClick={async () => {
+            const response = await futura_actor?.whoami();
+            console.log(response);
+            setMyString(response?.toString());
+          }}>
+            whoami
+            </button>
+            {myString}
+            <br / >
+              AUTH
+          <AuthButton authenticate={authenticate} deauthenticate={deauthenticate} />
+        </div>
+      </AuthContext.Provider>
     );
-  }
 
   // isAuthenticated = true
-  return (
-    <div>
-      <NavBar />
-      <ProfileCard />
-        <div className="main">
-          <div className="container">
-            {exampleposts.map((post) => (
-              <SinglePost
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                content={post.content}
-              />
-            ))}
-          </div>
-        </div>
-        <button onClick={deauthorize}>deAuthorize</button>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <NavBar />
+  //     <ProfileCard />
+  //       <div className="main">
+  //         <div className="container">
+  //           {exampleposts.map((post) => (
+  //             <SinglePost
+  //               key={post.id}
+  //               title={post.title}
+  //               author={post.author}
+  //               content={post.content}
+  //             />
+  //           ))}
+  //         </div>
+  //       </div>
+  //       {/* <button onClick={deauthorize}>deAuthorize</button> */}
+  //   </div>
+  // );
 }
 
 
-
+function AuthButton(props) {
+  const {isAuthenticated} = useContext(AuthContext);
+  return (
+    <button onClick={isAuthenticated ? props.deauthenticate : props.authenticate}>
+        {isAuthenticated ? 'DEAUTHENTICATE' : "AUTHENTICATE"}
+      </button>
+  );
+}
 
 
 
