92e48eebb93f5106bbba5c64887e8aaf0e05cc23
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2022-06-03 02:22:45 -0700

    refactor auth code & profilecard, add fetch ability to profilecard, add basic animations

--- src/symposium_futura_assets/src/index.tsx
+++ src/symposium_futura_assets/src/index.tsx
@@ -1,4 +1,4 @@
-import { /*symposium_futura,*/ canisterId, createActor } from "../../declarations/symposium_futura";
+import { symposium_futura, canisterId, createActor } from "../../declarations/symposium_futura";
 import { _SERVICE } from "../../declarations/symposium_futura/symposium_futura.did";
 import { AuthClient } from "@dfinity/auth-client";
 import { Actor, Identity, ActorSubclass, /*HttpAgent*/ } from "@dfinity/agent";
@@ -11,17 +11,32 @@ import exampleposts from "../assets/exampleposts.json";
 import '../assets/css/symposium.scss';
 
 
+
 const audio = new Audio("../ill-find-you-crystal-river-inmysleep.mp3");
 
 
 
+interface AuthContextInterface {
+  authClient: AuthClient | undefined,
+  isAuthenticated: boolean | undefined,
+  identity: Identity | undefined,
+  futura_actor: ActorSubclass<_SERVICE> | undefined
+}
 
 
-const AuthContext = React.createContext({
-  authClient: null,
+// const AuthContext = React.createContext({
+//   authClient: null,
+//   isAuthenticated: false,
+//   identity: null,
+//   futura_actor: null
+// });
+
+
+const AuthContext = React.createContext<AuthContextInterface>({
+  authClient: undefined,
   isAuthenticated: false,
-  identity: null,
-  futura_actor: null
+  identity: undefined,
+  futura_actor: undefined
 });
 
 
@@ -37,7 +52,6 @@ function App() {
   console.log("rerendered");
 
 
-
   useEffect(() => {
     console.log("useEffect just got called")
     const init = async () => {
@@ -47,20 +61,7 @@ function App() {
       setAuthClient(temp_authClient);
       setIsAuthenticated(temp_isAuthenticated);
 
-      // console.log(temp_isAuthenticated, isAuthenticated)
-
-
-      // if (temp_isAuthenticated) {
       handleAuthenticated(temp_authClient)
-      // }
-
-      // // kinda works?? but not on page refresh
-      // setAuthClient(await AuthClient.create())
-      // console.log(authClient);
-      // console.log("setisauthenticated: " + await authClient.isAuthenticated())
-      // setIsAuthenticated(await authClient?.isAuthenticated())
-      // console.log(isAuthenticated)
-
     };
     init();
   }, []);
@@ -95,7 +96,6 @@ function App() {
     const identity = temp_identity;
     setIdentity(temp_identity);
 
-
     const temp_futura_actor = createActor(canisterId as string, {
       agentOptions: {
         identity,
@@ -103,9 +103,9 @@ function App() {
     });
 
     setFutura_Actor(temp_futura_actor);
-    
-    // const response = await futura_actor1?.whoami();
-    // console.log(response?.toString())
+
+    const response = await futura_actor?.whoami();
+
 
     // Invalidate identity then render login when user goes idle
     authClient?.idleManager?.registerCallback(() => {
@@ -119,83 +119,37 @@ function App() {
     authClient?.logout();
     setIsAuthenticated(await authClient?.isAuthenticated());
     handleAuthenticated(authClient as AuthClient)
-    // flushSync(() => {
-    //   setIsAuthenticated(false);
-    // });
-    // setIsAuthenticated(false)
-    // setIdentity(undefined);
-    // setFutura_Actor(undefined);
   }
 
 
+  const loginAuthContext: AuthContextInterface = {
+    authClient: authClient,
+    isAuthenticated: isAuthenticated,
+    identity: identity,
+    futura_actor: futura_actor
+  }
+
   // Rendering 
     return (
-      <AuthContext.Provider value={{
-        authClient: authClient as any,
-        isAuthenticated: isAuthenticated as boolean,
-        identity: identity as any,
-        futura_actor: futura_actor as any,
-        }}>
-        <div>
-          <h1>authenticate options</h1>
-          Counter
-          <button onClick={() => setMyData(myData + 1)}>
-            Click me
-            </button>
-          {myData}
-          <br / >
-            <h4>State</h4>
-            {"authClient:   " + authClient} <br / >
-            {"isAuthenticated:  " + isAuthenticated} <br />
-            {"identity:  " + identity} <br / >
-            {"actor:  " + futura_actor + ", tostring: " + futura_actor?.toString()} <br />
-            <br />
-          {"authClient.getIdentity().getPrincipal():    " + String(authClient?.getIdentity().getPrincipal())}<br />
-          <button onClick={async () => {
-            const response = await futura_actor?.whoami();
-            console.log(response);
-            setMyString(response?.toString());
-          }}>
-            whoami
-            </button>
-            {myString}
-            <br / >
-              AUTH
-          <AuthButton authenticate={authenticate} deauthenticate={deauthenticate} />
+      <AuthContext.Provider value={loginAuthContext}>
+          <div>
+          <NavBar />
+          <ProfileCard authenticate={authenticate} deauthenticate={deauthenticate}/>
+            <div className="main" id="maincontent">
+              <div className="container">
+                {exampleposts.map((post) => (
+                <SinglePost
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                content={post.content}
+              />
+            ))}
+          </div>
         </div>
+      </div>
       </AuthContext.Provider>
     );
-
-  // isAuthenticated = true
-  // return (
-  //   <div>
-  //     <NavBar />
-  //     <ProfileCard />
-  //       <div className="main">
-  //         <div className="container">
-  //           {exampleposts.map((post) => (
-  //             <SinglePost
-  //               key={post.id}
-  //               title={post.title}
-  //               author={post.author}
-  //               content={post.content}
-  //             />
-  //           ))}
-  //         </div>
-  //       </div>
-  //       {/* <button onClick={deauthorize}>deAuthorize</button> */}
-  //   </div>
-  // );
-}
-
-
-function AuthButton(props) {
-  const {isAuthenticated} = useContext(AuthContext);
-  return (
-    <button onClick={isAuthenticated ? props.deauthenticate : props.authenticate}>
-        {isAuthenticated ? 'DEAUTHENTICATE' : "AUTHENTICATE"}
-      </button>
-  );
 }
 
 
@@ -225,6 +179,20 @@ function NavBar() {
 
 
 function ProfileCard(props) {
+  const {isAuthenticated, futura_actor} = useContext(AuthContext);
+  const [name, setName] = useState<string>();
+  const [id, setId] = useState<string>();
+
+  useEffect(() => {
+    const init = async () => {
+      setName("-----")
+      const response = await futura_actor?.whoami();
+      setId(response?.toString());
+      setName("EINSTEIN");
+    };
+    init();
+  }, [isAuthenticated]);
+
   const placeholders = [
     "when you're ready to change the world",
     "and be my guiding light",
@@ -233,38 +201,76 @@ function ProfileCard(props) {
   ];
 
   function animateIn() {
+    const elem = document.getElementById("maincontent");
+    elem!.animate([{
+      transform: 'translate(42px, 0px)',
+      filter: 'blur(10px)',
+    }], {
+      duration: 1000,
+      fill: 'forwards',
+      easing: 'ease-out',
+    })
+    // elem!.style.marginLeft = "300px";
+    // elem!.style.filter = "blur(1px)";
+    // elem!.style.transform = "translate(42px, 0px)";
     audio.play();
-    console.log("I don't do anything");
   }
 
   function animateOut() {
     //clearInterval(this.timerID);
+    const elem = document.getElementById("maincontent");
+    elem!.animate([{
+      transform: 'translate(0px, 0px)',
+      filter: 'blur(0px)',
+    }], {
+      duration: 0,
+      fill: 'forwards',
+      easing: 'ease-out',
+    })
+    // elem!.style.transform = "translate(0px, 0px)";
+    // elem!.style.filter = "blur(0px)";
     audio.pause();
     audio.load();
-    console.log("I don't do anything, but in reverse");
   }
 
   const pseudonym = props.pseudonym || "ANONYMOUS";
   const bio = props.bio || (<p>wake me<br />{rand_nth(placeholders)}</p>);
   const money = props.money || "$₣: 200 ∞";
-  
 
-  return (
-    <div className="sidebar">
-      <img src="img/hellorobotsomitsleep-cropped.jpeg" alt="profile" />
-      <h5 id="pseudonym">{pseudonym}</h5> 
-      <div id="bio">{bio}</div>
-      <p id="money">{money}</p>
-      <button
-        id="authButton"
-        // onClick={authorize}
-        onMouseOver={() => animateIn()}
-        onMouseOut={() => animateOut()}
-      >
-        Authorize {/*"Authenticate?"*/}
-      </button>
-    </div>
-  );
+
+  function AuthenticatedView() {
+    return (
+      <div className="sidebar">
+        <img src="img/hellorobotsomitsleep-cropped.jpeg" alt="profile" />
+        <p id="idbox">{id}</p>
+        <h5 id="pseudonym">{name}</h5>
+        <div id="bio">{bio}</div>
+        <p id="money">{money}</p>
+        <button id="authButton" onClick={props.deauthenticate}>DEAUTHENTICATE</button>
+      </div>
+    )
+  }
+
+  function UnauthenticatedView() {
+    return (
+      <div className="sidebar">
+        <img src="img/hellorobotsomitsleep-cropped.jpeg" alt="profile" />
+        <p id="idbox">{id}</p>
+        <h5 id="pseudonym">{pseudonym}</h5> 
+        <div id="bio">{bio}</div>
+        <p id="money">{money}</p>
+        <button id="authButton" onClick={props.authenticate}
+          onMouseOver={() => animateIn()}
+          onMouseOut={() => animateOut()}>AUTHENTICATE</button>
+      </div>
+    );
+  }
+  
+  if (isAuthenticated) {
+    return <AuthenticatedView />;
+  } else {
+    return <UnauthenticatedView />;
+  }
 }
 
 
