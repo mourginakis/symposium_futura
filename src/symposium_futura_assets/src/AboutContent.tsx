f6de2c63c2e764cbbbd27d5cbfcd3a55237f911d
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2022-06-11 14:54:34 -0700

    add proper about page

--- src/symposium_futura_assets/src/AboutContent.tsx
+++ src/symposium_futura_assets/src/AboutContent.tsx
@@ -1,5 +1,50 @@
 function AboutContent() {
-  return <p>about page!!!</p>;
+    return (
+        <>
+        <h3>ABOUT</h3>
+
+        <h3>a community sourced blog</h3>
+
+        <p>
+            <strong>{/*"An ideashare platform"*/} that uses a tokenomics system to incentivize 
+                profound original content sourced by an intellectual community</strong>
+        </p>
+        <h3>THE VISION</h3>
+        <p>
+            COMMUNITY OWNED INTELLECTUAL CONTENT <br />
+            Think <a href="https://lesswrong.com">LessWrong</a>, but with the 
+            ability to prompt, sponsor, vote, and tokenize posts and ideas.
+
+        </p>
+
+        <p>
+            this blog is likely to to change name to beyondpracticality <br />
+            This is powerful because it works in both ways -- authors can sell their posts as NFTs (non-fungible)
+            as well as 
+        </p>
+
+        <h5>TODO:</h5>
+        <ul>
+            <li>overhaul frontend with clojurescript, reagent-react, and tailwind</li>
+            <ul>
+                <li>radically more elegant, efficient and maintainable code</li>
+                <li>add post archival system, sort by tags, date, author</li>
+                <li>edit posts, create comments</li>
+                <li>more fleshed out user profiles</li>
+                <li>add sublime graphics and animations</li>
+            </ul>
+
+            <li>introduce tokenization</li>
+            <ul>
+                <li>readers/authors can prompt certain posts, or their own posts</li>
+                <li>readers can add cash bounty prizes to posts</li>
+                <li>post voting for most profound content</li>
+                <li></li>
+            </ul>
+            <li></li>
+        </ul>
+        </>
+    );
 }
 
 export default AboutContent;
